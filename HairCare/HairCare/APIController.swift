a233b96badf2b0a681b663dbdf553146132bf64b
Tobi Tselios <tobi.tselios@gmail.com>
2020-02-06 17:07:33 +0000

    Authentication and fetching methods complete

--- HairCare/HairCare/APIController.swift
+++ HairCare/HairCare/APIController.swift
@@ -16,12 +16,18 @@ enum HTTPMethod: String {
 
 enum HeaderNames: String {
     case contentType = "Content-Type"
+    case authorization = "Authorization"
 }
 
 enum NetworkError: Error {
     case encodingError
-    case unexpectedStatusCoded(Error)
+    case unexpectedStatusCode(Error)
     case requestError(Error)
+    case noBearer
+    case serverError(Error)
+    case badData
+    case noDecode
+    case otherError
 }
 
 class APIController {
@@ -29,9 +35,13 @@ class APIController {
     private let baseURL = URL(string: "https://haircare1backend.herokuapp.com/")!
     var bearer: Bearer?
     
-    typealias SignInCompletonHandler = (NetworkError?) -> Void
+    typealias SignInCompletionHandler = (NetworkError?) -> Void
+    typealias StylistCitiesCompletionHandler = (Result<[StylistRepresentation], NetworkError>) -> Void
+    typealias AllStylistsCompletionHandler = (Result<[StylistRepresentation], NetworkError>) -> Void
+    typealias StylistDetailsCompletionHandler = (Result<StylistRepresentation, NetworkError>) -> Void
+    typealias StylistImageCompletionHandler = (Result<UIImage, NetworkError>) -> Void
     
-    func signUp(customer: CustomerRepresentation, completion: @escaping SignInCompletonHandler) {
+    func signUp(customer: CustomerRepresentation, completion: @escaping SignInCompletionHandler) {
         let requestURL = baseURL
         .appendingPathComponent("api")
         .appendingPathComponent("customers")
@@ -58,13 +68,13 @@ class APIController {
             if let response = response as? HTTPURLResponse,
                 response.statusCode != 200 {
                 let statusCode = NSError(domain: "", code: response.statusCode, userInfo: nil)
-                completion(.unexpectedStatusCoded(statusCode))
+                completion(.unexpectedStatusCode(statusCode))
             }
             completion(nil)
         }.resume()
     }
     
-    func signIn(customer: CustomerRepresentation, completion: @escaping SignInCompletonHandler) {
+    func signIn(customer: CustomerRepresentation, completion: @escaping SignInCompletionHandler) {
         let requestURL = baseURL
         .appendingPathComponent("api")
         .appendingPathComponent("customers")
@@ -91,9 +101,115 @@ class APIController {
             if let response = response as? HTTPURLResponse,
                 response.statusCode != 200 {
                 let statusCodeError = NSError(domain: "", code: response.statusCode, userInfo: nil)
-                completion(.unexpectedStatusCoded(statusCodeError))
+                completion(.unexpectedStatusCode(statusCodeError))
             }
             completion(nil)
+        }.resume()
+    }
+    
+    func fetchStylists(in city: String, completion: @escaping StylistCitiesCompletionHandler) {
+        guard let bearer = bearer else {
+            completion(.failure(.noBearer))
+            return
+        }
+        
+        let stylistsURL = baseURL
+        .appendingPathComponent("api")
+        .appendingPathComponent("stylists")
+        .appendingPathComponent("location")
+        .appendingPathComponent(city)
+        
+        var request = URLRequest(url: stylistsURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: HeaderNames.authorization.rawValue)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                NSLog("Error fetching stylists in \(city): \(error)")
+                completion(.failure(.serverError(error)))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                completion(.failure(.unexpectedStatusCode(NSError())))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            do {
+                let stylists = try JSONDecoder().decode([StylistRepresentation].self, from: data)
+                completion(.success(stylists))
+            } catch {
+                NSLog("Error decoding stylists in \(city): \(error)")
+                completion(.failure(.noDecode))
+            }
+        }.resume()
+    }
+    
+    func fetchAllStylists(completion: @escaping AllStylistsCompletionHandler) {
+        guard let bearer = bearer else {
+            completion(.failure(.noBearer))
+            return
+        }
+        
+            let stylistURL = baseURL
+            .appendingPathComponent("api")
+            .appendingPathComponent("stylists")
+            
+            var request = URLRequest(url: stylistURL)
+            request.httpMethod = HTTPMethod.get.rawValue
+            request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: HeaderNames.authorization.rawValue)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                NSLog("Error fetching stylists: \(error)")
+                completion(.failure(.serverError(error)))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                completion(.failure(.unexpectedStatusCode(NSError())))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            do {
+                let stylists = try JSONDecoder().decode([StylistRepresentation].self, from: data)
+                completion(.success(stylists))
+            } catch {
+                NSLog("Error decoding stylists: \(error)")
+                completion(.failure(.noDecode))
+            }
+        }.resume()
+    }
+    
+    func fetchImage(at urlString: String, completion: @escaping StylistImageCompletionHandler) {
+        let imageURL = URL(string: urlString)!
+        let request = URLRequest(url: imageURL)
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let _ = error {
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            let image = UIImage(data: data)!
+            completion(.success(image))
         }.resume()
     }
 }
